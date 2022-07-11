d026fab3a7173ad9fafdbc11e424902a386ce5f2
Pradeep kaswan <pradeepkaswan@Pradeeps-MacBook-Pro.local>
2022-07-11 10:51:56 +0530

    add navbar component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,14 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import { ChakraProvider } from '@chakra-ui/react'
+import type { AppProps } from 'next/app';
+import { ChakraProvider } from '@chakra-ui/react';
+import { theme } from '../chakra/theme' ;
+import Layout from '../components/Layout/Layout';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
+      <Layout>
       <Component {...pageProps} />
+      </Layout>
     </ChakraProvider>
   )
 }
